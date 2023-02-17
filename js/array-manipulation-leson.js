3292d55f29a390c6157120e2ce951c53175553f8
Kelvyn Medellin <medellinkelvyn98@gmail.com>
2023-02-17 14:05:48 -0600

    Turning in the exercises and uploading notes from Arrays ( Multiple JS Files )

--- js/array-manipulation-leson.js
+++ js/array-manipulation-leson.js
@@ -1,47 +1,47 @@
-`use strict`;
-
-const daysOfWeek = [`Monday ` , `Tuesday` , `Wednesday` , `Thursday`, `Friday` , `Saturday`, `Sunday`]
-
-
-/** indexOf shows only the firdt one it runs into */
-const myName = `Jason`;
-const sPosition = myName.indexOf(`s`)
-//console.log(sPosition)
-
-const myNameArray = myName.split(``)
-//console.log(myNameArray)
-
-const thursday = daysOfWeek.indexOf(`Thursday`);
-//console.log(thursday)
-
-const removeSunday = daysOfWeek.pop();
+// `use strict`;
+//
+// const daysOfWeek = [`Monday ` , `Tuesday` , `Wednesday` , `Thursday`, `Friday` , `Saturday`, `Sunday`]
+//
+//
+// /** indexOf shows only the firdt one it runs into */
+// const myName = `Jason`;
+// const sPosition = myName.indexOf(`s`)
+// //console.log(sPosition)
+//
+// const myNameArray = myName.split(``)
+// //console.log(myNameArray)
+//
+// const thursday = daysOfWeek.indexOf(`Thursday`);
+// //console.log(thursday)
+//
+// const removeSunday = daysOfWeek.pop();
+// // console.log(daysOfWeek)
+// // console.log(removeSunday)
+//
+// const removeFirst = daysOfWeek.shift();
 // console.log(daysOfWeek)
-// console.log(removeSunday)
-
-const removeFirst = daysOfWeek.shift();
-console.log(daysOfWeek)
-console.log(removeFirst)
-
-///////slicing
-
-
-
-//removing an item in the middle of the string
-const indexOfThusday = daysOfWeek.indexOf(`Thursday`);
-const lastHaf = daysOfWeek.slice(indexOfThusday + 1);
-daysOfWeek = daysOfWeek.slice(0, indexOfThusday);
-
-/** SPREAD OPERATOR */
-
-daysOfWeek = [
-    ...firstHalf,
-        ... lastHalf,
-    `Merrelday`
-]
-
-daysOfWeek.splice( indexOfThusday, 1, `Merelldya`)
-console.log()
-
-
-SPLICE CAN TAKE 3 ARGUMENTS
-
+// console.log(removeFirst)
+//
+// ///////slicing
+//
+//
+//
+// //removing an item in the middle of the string
+// const indexOfThusday = daysOfWeek.indexOf(`Thursday`);
+// const lastHaf = daysOfWeek.slice(indexOfThusday + 1);
+// //daysOfWeek = daysOfWeek.slice(0, indexOfThusday);
+//
+// /** SPREAD OPERATOR */
+//
+// //daysOfWeek = [
+//     ...firstHalf,
+//         ... lastHalf,
+//     `Merrelday`
+// ]
+//
+// daysOfWeek.splice( indexOfThusday, 1, `Merelldya`)
+// console.log()
+//
+//
+// SPLICE CAN TAKE 3 ARGUMENTS
+//
